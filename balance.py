3f69df88dd6d6b340f1735ac265328688f614152
saya3838 <86864442+saya3838@users.noreply.github.com>
2022-03-12 22:06:44 +0900

    if文の書き方とf文字列

--- balance.py
+++ balance.py
@@ -1,6 +1,6 @@
 initial_balance = int(input("残高により利子率が変わります。残高はおいくらですか? "))
 RATE = 1.1 if initial_balance >= 100_000 else 1.01  #この書き方の場合、最後に：は必要ない。
 print(f"残高から、利子率は{RATE:.2f}％になります。")
-print(f"0年: {initial_balance:,.2f}円")
-print(f"1年: {initial_balance * RATE:,.2f}円")
-print(f"2年: {initial_balance * RATE * RATE:,.2f}円")
+after = int(input("何年後まで計算しますか？"))
+for year in range(0, after+1):
+  print(f"{year}年: {initial_balance * RATE ** year:,.2f}円")
