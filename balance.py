7d38a57e6ea3b3a5dfe4f58c9eea09d4e43e0b4e
saya3838 <86864442+saya3838@users.noreply.github.com>
2022-03-12 21:35:43 +0900

    残高計算

---
+++ balance.py
@@ -0,0 +1,6 @@
+initial_balance = int(input("残高により利子率が変わります。残高はおいくらですか? "))
+RATE = 1.1 if initial_balance >= 100_000 else 1.01
+print(f"残高から、利子率は{RATE:.2f}％になります。")
+print(f"0年: {initial_balance:,.2f}円")
+print(f"1年: {initial_balance * RATE:,.2f}円")
+print(f"2年: {initial_balance * RATE * RATE:,.2f}円")
